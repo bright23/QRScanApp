6127ee75982bb6db222a12c60d4e16781a711520
Wataru Fujiwara <wataru930@gmail.com>
2021-08-17 19:34:06 +0900

    Initial Commit

---
+++ QRScanApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QRScanApp
+//
+//  Created by Wataru Fujiwara on 2021/08/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QRScanApp/QRScanAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRScanAppApp.swift
+//  QRScanApp
+//
+//  Created by Wataru Fujiwara on 2021/08/17.
+//
+
+import SwiftUI
+
+@main
+struct QRScanAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
